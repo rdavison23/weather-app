2299e02d47159650874303c18a4b779dc5fb3107
Your Name <your-email@example.com>
2026-01-28 23:13:10 -0800

    imported day/night images. Implemented isDaytome() to detect local time

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,21 @@ import './App.css';
 import { useState } from 'react';
 import WeatherForm from './components/weatherForm';
 import WeatherCard from './components/weatherCard';
+import dayImage from './components/day.jpg';
+import nightImage from './components/night.jpg';
 
 function App() {
   const [city, setCity] = useState('');
   const [result, setResult] = useState(null);
+
+  const isDaytime = () => {
+    if (!result) return true; //defult before data loads
+    const localUnix = result.dt + result.timezone;
+    const localDate = new Date(localUnix * 1000);
+    const hour = localDate.getUTCHours();
+
+    return hour >= 6 && hour < 18;
+  };
 
   //function to do the get request and set the state from the hard code data
   const loadCity = () => {
@@ -37,7 +48,18 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${
+          result ? (isDaytime() ? dayImage : nightImage) : dayImage
+        })`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        minHeight: '100vh',
+        width: '100%',
+      }}>
       <WeatherForm setCity={setCity} handleSubmit={handleSubmit} />
       {!result ? (
         <p>Please click the button to see Data</p>
