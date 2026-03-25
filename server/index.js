545cc012456097dbbada253781ae81dbaee73ce8
Your Name <your-email@example.com>
2026-03-24 18:53:01 -0700

    cleaned up index.js removed dependency on clinet

--- server/index.js
+++ server/index.js
@@ -1,25 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import weatherRouter from './routes/weather.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const server = express();
-const REACT_BUILD_DIR = path.join(__dirname, '..', 'client', 'build');
-
 server.use(express.json());
-server.use(express.static(REACT_BUILD_DIR));
 
 // API routes
 server.use('/weather', weatherRouter);
 
 // Serve React app
 server.get('/', (req, res) => {
-  res.sendFile(path.join(REACT_BUILD_DIR, 'index.html'));
+  res.send('Okay!');
 });
 
 // Basic error handler
