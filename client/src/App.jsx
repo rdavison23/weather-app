3d8bb74fde5c05dbe55f3abb49498c248d0277b8
Your Name <your-email@example.com>
2026-03-25 20:55:02 -0700

    converted app.jsx to user browser router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import WeatherView from './components/WeatherView';
 
 function App() {
   return (
-    <main>
-      <WeatherView />
-    </main>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<WeatherView />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
