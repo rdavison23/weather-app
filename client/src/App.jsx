89a72bd13d8924842bfbb5957411057651f2a75d
Your Name <your-email@example.com>
2026-03-26 10:13:11 -0700

    update weatherview and add registerView rote

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import WeatherView from './components/WeatherView';
+import WeatherView from './views/WeatherView';
+import RegisterView from './views/RegisterView';
 
 function App() {
   return (
     <BrowserRouter>
       <nav>
         <Link to="/">Home</Link>
+        <Link to="/register">Register</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<WeatherView />} />
+        <Route path="/register" element={<RegisterView />} />
       </Routes>
     </BrowserRouter>
   );
